22a26a503c9fa810349ecbf3f249565e34ae23ae
Akash Shinde <saakashs1999@gmail.com>
2024-11-11 17:51:45 +0530

    updated

---
+++ before_install.sh
@@ -0,0 +1,7 @@
+cd /home/ec2-user/angular-frontend
+
+rm -rf *
+
+unzip -o /home/ec2-user/angular-frontend/demo.zip -d /home/ec2-user/angular-frontend/
+
+rm /home/ec2-user/angular-frontend/demo.zip
